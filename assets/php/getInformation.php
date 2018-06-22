b3d565d79a7cefdfcdd1b044a3eea94f83b70aa7
Marvin Borner <marvin-borner@live.com>
2018-06-23 00:38:55 +0200

    Revert "Using different font and created gradient markup"
    This reverts commit f7d128440cde0fafce2d4db18c41c844da09682c.

--- assets/php/getInformation.php
+++ assets/php/getInformation.php
@@ -5,10 +5,10 @@
  * @copyright Marvin Borner 2018
  */
 
-$RequestedTitle = $_POST["Title"];
+$RequestedTitle = $_GET["Title"];
 
 $ApiKey = file_get_contents("../../../../ApiKeys/ThemoviedbApiKey.txt");
-$ch = curl_init("https://api.themoviedb.org/3/search/multi?api_key=" . $ApiKey . "&language=en-US&query=" . urlencode($RequestedTitle) . "&page=1&include_adult=true");
+$ch = curl_init("https://api.themoviedb.org/3/search/multi?api_key=" . $ApiKey . "&language=en-US&query=" . $RequestedTitle . "&page=1&include_adult=true");
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_HEADER, 0);
 $Result = json_decode(curl_exec($ch), true);
