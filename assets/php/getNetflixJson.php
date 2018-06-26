b0da1293d77a40d60a5d172c063195a4f3fcd09a
Marvin Borner <marvin-borner@live.com>
2018-06-26 18:27:32 +0200

    Moved to SASS and some reformatting

--- assets/php/getNetflixJson.php
+++ assets/php/getNetflixJson.php
@@ -7,23 +7,23 @@
 
 $NetflixCookie = $_POST["Cookie"];
 
-$LastPage = FALSE;
+$LastPage = false;
 $CurPage = 0;
 $NetflixJson = "[";
 
-while ($LastPage === FALSE) {
-    $ch = curl_init("https://www.netflix.com/api/shakti/7742b8c7/viewingactivity?pg=" . (string)$CurPage . "&pgSize=100");
+while ($LastPage === false) {
+    $ch = curl_init("https://www.netflix.com/api/shakti/7742b8c7/viewingactivity?pg=" . (string) $CurPage . "&pgSize=100");
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     curl_setopt($ch, CURLOPT_HEADER, 0);
     curl_setopt($ch, CURLOPT_COOKIE, $NetflixCookie);
     $Result = curl_exec($ch);
     //print_r((json_decode($Result, TRUE)["viewedItems"]));
     //echo count(json_decode(curl_exec($ch),true)["viewedItems"]);
-    if ($LastPage = count(json_decode($Result, TRUE)["viewedItems"]) > 0) {
-        $LastPage = FALSE;
-        $NetflixJson .= json_encode(json_decode($Result, TRUE)["viewedItems"]) . ",";
+    if ($LastPage = count(json_decode($Result, true)["viewedItems"]) > 0) {
+        $LastPage = false;
+        $NetflixJson .= json_encode(json_decode($Result, true)["viewedItems"]) . ",";
     } else {
-        $LastPage = TRUE;
+        $LastPage = true;
         $NetflixJson = substr($NetflixJson, 0, -1);
     }
 
